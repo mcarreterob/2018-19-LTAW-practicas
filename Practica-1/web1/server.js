fa4d5694e231d9445de0a0b43dada13c34999305
Mario Carretero Berrocal <mariocb@delta17.aulas.gsyc.urjc.es>
2019-02-04 12:41:21 +0100

    4/2 12.41

---
+++ Practica-1/web1/server.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+console.log("Arrancando servidor...")
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+  console.log("Peticion atendida")
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data){
+    if (err) {
+      res.writeHead(404, {'Content_Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+    console.log("Peticion atendida")
+  });
+}).listen(8080);
