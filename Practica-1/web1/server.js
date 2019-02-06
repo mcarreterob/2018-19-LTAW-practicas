ee738a759647d0b2fc7e7cd3b4cb8e1430c0ae89
Mario Carretero <m.carreterob@alumnos.urjc.es>
2019-02-06 19:41:00 +0100

    Server responde peticiones

--- Practica-1/web1/server.js
+++ Practica-1/web1/server.js
@@ -4,7 +4,22 @@ var fs = require('fs');
 
 console.log("Arrancando servidor...")
 
-
+http.createServer((req, res) => {
+  console.log("---> Peticion recibida");
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data){
+    console.log("Recurso solicitado (URL): " + req.url);
+    if (err) {
+      res.writeHead(404, {'Content_Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+    res.write(data);
+    return res.end();
+    console.log("Peticion atendida");
+  });
+}).listen(8080);
+/*
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
@@ -20,5 +35,6 @@ http.createServer(function (req, res) {
     res.write(data);
     return res.end();
     console.log("Peticion atendida")
-  });
+  })
 }).listen(8080);
+*/
