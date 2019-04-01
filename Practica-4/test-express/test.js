6ecc22467d7833f1d82d03524a25a6e946eb261b
Mario Carretero Berrocal <mariocb@beta18.aulas.gsyc.urjc.es>
2019-04-01 12:54:23 +0200

    1/4 15:54

--- Practica-4/test-express/test.js
+++ Practica-4/test-express/test.js
@@ -26,11 +26,20 @@ http.listen(PORT, function(){
 
 //-- Evento: Nueva conexion recibida
 //-- Un nuevo cliente se ha conectado
+
+var clients= [];
 io.on('connection', function(socket){
   console.log('--> Usuario conectado \r\n');
+  clients.push(socket.id);
   //-- Detectar si el usuario se ha desconectado
+  io.emit('new_message', 'Hay un nuevo usuario, bienvenido!');
+  console.log(clients);
   socket.on('disconnect', function(){
     console.log('--> Usuario desconectado');
+    disconnected = clients.filter(clients[i] == socket.id)
+    console.log(disconnected);
+    clients.pop(socket.id);
+    console.log(clients);
   });
   //-- Detectar si se ha recibido un mensaje del cliente
   socket.on('new_message', msg => {
