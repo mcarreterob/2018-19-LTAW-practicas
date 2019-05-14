72eec0b2d3bfdefe9eedb7e0145b2ef33caa9525
Mario Carretero Berrocal <mariocb@alpha09.aulas.gsyc.urjc.es>
2019-05-14 17:56:36 +0200

    P4 con names y funcionando to guapo

--- Practica-4/test-express/chat-client.js
+++ Practica-4/test-express/chat-client.js
@@ -1,5 +1,5 @@
 function main(){
-  console.log("Hola!!!!");
+  var user_name = prompt('Who are you?');
   //-- Crear un socket.io. Se establece
   //-- la conexion con el Servidor
   var socket = io();
@@ -11,6 +11,7 @@ function main(){
   var display = document.getElementById('display')
   //-- Caja con el mensaje a enviar
   var msg = document.getElementById('msg')
+  var user = document.getElementById('user')
 
   // -- Enviar el mensaje pulsando la tecla ENTER
   msg.addEventListener("keyup", function(event){
@@ -20,12 +21,14 @@ function main(){
     }
   })
 
+  socket.emit('new_name', user_name)
+
   send.onclick = () => {
     //-- Enviamos el mensaje
-    socket.emit('new_message', msg.value);
+    socket.emit('new_message', user_name + ": " +  msg.value);
     //-- Lo notificamos en la consola del servidor
     console.log("Mensaje emitido");
-    msg.value = " ";
+    msg.value = "";
   }
   //-- Cuando se reciba un mensaje del
   //-- servidor se muestra en el parrafo
